d03b99490b0114228d2fb9152ed47f09872c83d8
GCU New Hostel <newhostel@gcu.edu.pk>
2024-02-17 17:54:00 +0500

    Refactor contactUs.jsx to use emailjs for sending emails

--- src/Components/ContactUs/contactUs.jsx
+++ src/Components/ContactUs/contactUs.jsx
@@ -1,59 +1,63 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faPhone, faPaperPlane, faEarthAmericas } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot, faPhone, faPaperPlane, faEarthAmericas} from "@fortawesome/free-solid-svg-icons";
 import './contactUs.css'
-import { useState } from "react";
-import { showSuccessToast } from "../Toast/ToastifyToast";
+import emailjs from '@emailjs/browser';
+import React, { useRef} from 'react'
 
 
+  
 
+function ContactUs(){
 
-function ContactUs() {
-
-    const [formData, setFormData] = useState({ name: '', email: '', subject: '', message: '' })
-
-    const updateFormData = (key, value) => {
-        setFormData(prevData => ({ ...prevData, [key]: value }))
-    }
+    const form = useRef();
 
     const sendEmail = (e) => {
-        e.preventDefault();
-
-        console.log(formData)
-        setFormData({ name: '', email: '', subject: '', message: '' })
-        showSuccessToast("Message Sent Successfully")
+      e.preventDefault();
+  
+      emailjs.sendForm('service_r3lhvx8', 'template_p6tl1fa', form.current, {
+          publicKey: '02kIoBq_XrkSX3G0y',
+        })
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error.text);
+          },
+        );
     };
 
 
-    return (
+    return(
         <div className='contactUs_main'>
             <div className='contactUs_container'>
                 <div className='contactUs_containerBox'>
                     <div className='contactUs_form_map'>
                         <div className='contactUs_formMain'>
-                            <form className="contactUs_form" onSubmit={sendEmail}>
+                            <form className="contactUs_form" ref={form} onSubmit={sendEmail}>
                                 <div className='contactUs_Heading'>
                                     <p>Contact Us</p>
                                 </div>
                                 <div className='contactUs_nameEmail'>
                                     <div className='contactUs_Name'>
                                         <label htmlFor="contactUs_fullName">Full Name</label>
-                                        <input value={formData.name} onChange={e => updateFormData("name", e.target.value)} className='contactUs_NameInputBar' type='text' id="" name="from_name" placeholder='Name' required />
+                                        <input className='contactUs_NameInputBar' type='text' id="" name="from_name" placeholder='Name' required />
                                     </div>
                                     <div className='contactUs_Email'>
                                         <label htmlFor="">Email Address</label>
-                                        <input value={formData.email} onChange={e => updateFormData("email", e.target.value)} className='contactUs_EmailInputBar' type='text' id="" name="from_email" placeholder='Email' required />
+                                        <input className='contactUs_EmailInputBar' type='text' id="" name="from_email" placeholder='Email' required />
                                     </div>
                                 </div>
                                 <div className='contactUs_Subject'>
                                     <label htmlFor="">Subject</label>
-                                    <input value={formData.subject} onChange={e => updateFormData("subject", e.target.value)} className='contactUs_SubjectInputBar' type='text' id="" name="" placeholder='Subject' required />
+                                    <input className='contactUs_SubjectInputBar' type='text' id="" name="" placeholder='Subject' required />
                                 </div>
                                 <div className='contactUs_Message'>
                                     <label htmlFor="">Message</label>
-                                    <textarea value={formData.message} onChange={e => updateFormData("message", e.target.value)} className='contactUs_MessageInputBar' name="message" id="" cols="30" rows="5" placeholder='Message'></textarea>
+                                    <textarea className='contactUs_MessageInputBar' name="message" id="" cols="30" rows="5" placeholder='Message'></textarea>
                                 </div>
                                 <div className='contactUs_button'>
-                                    <button type="submit" >Send Message</button>
+                                    <button ref={form} onClick={sendEmail}>Send Message</button>
                                 </div>
                             </form>
                         </div>
