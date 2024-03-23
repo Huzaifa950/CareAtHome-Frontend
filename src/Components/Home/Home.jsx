c1a61ff96e72458651492f8f71c8ed5c6ba85215
GCU New Hostel <newhostel@gcu.edu.pk>
2024-03-23 16:06:21 +0500

    Add Profile component to Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,3 @@
-import { ApiGetCall } from "../ApiCall/ApiCalls";
-import { showSuccessToast } from "../Toast/ToastifyToast";
 import Header from "../Header/header";
 import HomeInterface from "../HomeInterface/HomeInterface";
 import AboutUs from "../AboutUs/aboutUs";
@@ -7,7 +5,7 @@ import FAQs from "../FAQs/FAQs";
 import ContactUs from "../ContactUs/contactUs"
 import Footer from "../Footer/footer";
 import { Route, Routes } from "react-router-dom";
-import { Button, Dropdown, Nav, NavDropdown, Navbar, Stack } from "react-bootstrap";
+import Profile from "../Profile/Profile";
 
 const Home = ({ updateLogInStatus, userInfo }) => {
 
@@ -20,6 +18,7 @@ const Home = ({ updateLogInStatus, userInfo }) => {
                 <Route path="/aboutus" element={<AboutUs />} />
                 <Route path="/faqs" element={<FAQs />} />
                 <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
 
             <Footer />
---
+++ src/Components/Profile/CareTakerProfile.jsx
@@ -0,0 +1,11 @@
+import CommonProfile from "./CommonProfile";
+
+const CareTakerProfile = () => {
+    return (
+        <div>
+            <CommonProfile pageName="CareTaker" />
+        </div>
+    )
+}
+
+export default CareTakerProfile;
