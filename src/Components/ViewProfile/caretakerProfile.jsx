f91f3c7a563fcd0794399243760bcc0ec7c88d56
hassanAssembly <hassan.alvi@assembly-talent.com>
2024-05-26 15:42:37 +0500

    View Profile PopUp

--- src/Components/ViewProfile/caretakerProfile.jsx
+++ src/Components/ViewProfile/caretakerProfile.jsx
@@ -1,25 +1,99 @@
-import React from 'react';
-import './caretakerProfile.css';
-import { capitalizeEachWord } from '../Common/Common';
+import React, { useState } from "react";
+import "./caretakerProfile.css";
+import { capitalizeEachWord } from "../Common/Common";
 
 const CaretakerProfiles = ({ profiles, errorType }) => {
+    const [showFullDescription, setShowFullDescription] = useState(false);
+
+    const toggleDescription = () => {
+        setShowFullDescription(!showFullDescription);
+    };
+
+    const [showPopup, setShowPopup] = useState(false);
+
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    };
+
     return (
         <div className="container">
-            {errorType === "length" ? <h2 style={{ textAlign: "center" }}>No caretakers found</h2> :
-                errorType === "error" ? <h2 style={{ textAlign: "center" }}>Something went wrong. Please try again later.</h2> :
-
-                    <div className="caretaker-profiles">
-                        {profiles.map((profile, index) => (
-                            <div className="caretaker-profile" key={index}>
-                                <img src={profile.image} alt={profile.fullName} />
-                                <h3>{capitalizeEachWord(profile.fullName)}</h3>
-                                <p>{profile.description}</p>
+            {errorType === "length" ? (
+                <h2 style={{ textAlign: "center" }}>No caretakers found</h2>
+            ) : errorType === "error" ? (
+                <h2 style={{ textAlign: "center" }}>
+                    Something went wrong. Please try again later.
+                </h2>
+            ) : (
+                <div className="caretaker-profiles">
+                    {profiles.map((profile, index) => (
+                        <div className="caretaker-profile" key={index}>
+                            <div class='viewProfile' style={{textAlign:'right'}}>
+                                <button style={{fontSize:'12px', padding:'5px 8px', border:'none', borderRadius:'5px'}} onClick={togglePopup}> View Profile</button>
                             </div>
-                        ))}
-                    </div>
-            }
+                            <img src={profile.image} alt={profile.fullName} />
+                            <h3>{capitalizeEachWord(profile.fullName)}</h3>
+
+                            <div
+                                style={{
+                                    display: "flex",
+                                    justifyContent: "space-around",
+                                    marginTop: "80px",
+                                }}
+                            >
+                                <div style={{ textAlign: "left", maxWidth: "60%" }}>
+                                    <h5>Description</h5>
+                                    <p
+                                        style={{
+                                            marginLeft: "25px",
+                                            color: profile.description.length < 1 ? "grey" : "black",
+                                        }}
+                                    >
+                                        {profile.description.length < 1 ? (
+                                            "Empty"
+                                        ) : profile.description.split(" ").length > 30 ? (
+                                            <>
+                                                {showFullDescription
+                                                    ? profile.description
+                                                    : profile.description.split(' ').slice(0, 50).join(' ') + '...'}
+                                                <span
+                                                    onClick={toggleDescription}
+                                                    style={{ color: "blue", cursor: "pointer" }}
+                                                >
+                                                    {showFullDescription ? " show less" : " more"}
+                                                </span>
+                                            </>
+                                        ) : (
+                                            profile.description
+                                        )}
+                                    </p>
+                                </div>
+
+                                <div style={{ textAlign: "left" }}>
+                                    <h5>Phone No.</h5>
+                                    <p style={{ marginLeft: '25px', color: profile?.phoneNumber?.trim() === '' ? 'grey' : 'black' }}>
+                                        {profile?.phoneNumber?.trim() === '' ? 'Empty' : profile.phoneNumber}
+                                    </p>
+                                    <h5>Skills</h5>
+                                    <p style={{ marginLeft: '25px', color: profile?.skills?.trim() === '' ? 'grey' : 'black' }}>
+                                        {profile?.skills?.trim() === '' ? 'Empty' : profile.skills}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+
+                    {showPopup && (
+                        <div className="popup">
+                            <span className="close-icon" onClick={togglePopup}>
+                                &times;
+                            </span>
+                            {/* Add more content here */}
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     );
-}
+};
 
 export default CaretakerProfiles;
--- src/Components/ViewProfile/showCase.jsx
+++ src/Components/ViewProfile/showCase.jsx
@@ -4,7 +4,8 @@ import showcaseImage from '../../Assets/Images/search2.jpg';
 
 const Showcase = () => {
     return (
-        <section id="showcase" style={{backgroundImage: `url(${showcaseImage})`}}>
+        <section id="showcase" style={{background:'linear-gradient(180deg, rgba(25, 132, 134, 0.849), rgba(71, 49, 196, 0.788))', marginTop:'50px'}}>
+            {/* backgroundImage: `url(${showcaseImage})` */}
             <div className="container">
                 <h1>Find the Perfect Caretaker</h1>
                 <p>Search and view profiles of experienced caretakers for your needs.</p>
