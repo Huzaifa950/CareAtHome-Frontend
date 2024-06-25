863994e69aea293c4a7384254e99c411f258bfe2
GCU New Hostel <newhostel@gcu.edu.pk>
2024-06-25 17:39:26 +0500

    Booking message inside chatbot

--- src/Components/ApiCall/ApiCalls.jsx
+++ src/Components/ApiCall/ApiCalls.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3500/api";
+const baseUrl = "http://localhost:4000/api";
 axios.defaults.withCredentials = true;
 
 const headers = { "Content-Type": "application/json" };
